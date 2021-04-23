493fca36a4709337a29a96cb7c6737f3538442fe
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-04-23 13:42:17 -0300

    Done

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       brand: "Vue Mastery",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: false,
+      selectedVariant: 0, // The index of the current hovered
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -24,8 +33,20 @@ const app = Vue.createApp({
         this.cart -= 1;
       }
     },
-    updateImage(image) {
-      this.image = image;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      console.log(this.variants[this.selectedVariant].quantity);
+      return this.variants[this.selectedVariant].quantity > 0;
     },
   },
 });
